12592a026f303ca9fb021cc2cba75601c8d5e175
aldhinn <aldespinas@gmail.com>
2024-11-07 11:25:41 +0800

    building amd64 linux base image.

--- build.bat
+++ build.bat
@@ -14,6 +14,10 @@ if %errorlevel% neq 0 (
 @rem The directory where this script exists.
 set "script_dir=%~dp0"
 
+@rem Build amd64 linux base image.
+%docker_exe% build -t gh-self:amd64-linux-base --build-arg RUNNER_VERSION=2.320.0 \
+    --build-arg GH_PLATFORM=x64 %script_dir%\linux
+
 endlocal
 @rem End of line.
 @rem DO NOT WRITE BEYOND HERE.
